a5b2b2c6b529fdfefb74dbbd9bbc5c2027c82213
Akshay Jain <56107242+1880akshay@users.noreply.github.com>
2020-08-24 17:09:11 +0530

    more apis added

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+//student login related apis
+
 router.post('/studentLogin', function(req, res, next) {
     var user = req.body;
     let query = "SELECT * FROM students WHERE username='"+user.username+"' AND password='"+user.password+"'";
@@ -54,6 +56,70 @@ router.post('/studentIsPhoneNumber', function(req, res, next) {
 router.post('/studentIsUsername', function(req, res, next) {
     var username = req.body.username;
     let query = "SELECT * FROM students WHERE username='"+username+"'";
+    con.query(query, (err, result) => {
+        if(err) throw err;
+        else {
+            if(result && result.length) res.send({'status': 'notAvailable'});
+            else res.send({'status': 'available'});
+        }
+    })
+})
+
+//teacher login related apis
+
+router.post('/teacherLogin', function(req, res, next) {
+    var user = req.body;
+    let query = "SELECT * FROM teachers WHERE username='"+user.username+"' AND password='"+user.password+"'";
+    con.query(query, (err, result) => {
+        if(err) throw err;
+        else {
+            if(result && result.length) res.send({'status': 'success'});
+            else res.send({'status': 'failure'});
+        }
+    })
+})
+
+router.post('/teacherSignup', function(req, res, next) {
+  var user = req.body;
+  let query = "INSERT INTO teachers (name, email, phoneNumber, username, password) VALUES ('"+user.name+"', '"+user.email+"', '"+user.phoneNumber+"', '"+user.username+"', '"+user.password+"')";
+  con.query(query, (err, result) => {
+      if(err) {
+        res.send({'status': 'failure'});
+      }
+      else {
+        console.log('teacher record inserted');
+        res.send({'status': 'success'});
+      }
+  })
+});
+
+router.post('/teacherIsEmail', function(req, res, next) {
+    var email = req.body.email;
+    let query = "SELECT * FROM teachers WHERE email='"+email+"'";
+    con.query(query, (err, result) => {
+        if(err) throw err;
+        else {
+            if(result && result.length) res.send({'status': 'notAvailable'});
+            else res.send({'status': 'available'});
+        }
+    })
+})
+
+router.post('/teacherIsPhoneNumber', function(req, res, next) {
+    var phoneNumber = req.body.phoneNumber;
+    let query = "SELECT * FROM teachers WHERE phoneNumber='"+phoneNumber+"'";
+    con.query(query, (err, result) => {
+        if(err) throw err;
+        else {
+            if(result && result.length) res.send({'status': 'notAvailable'});
+            else res.send({'status': 'available'});
+        }
+    })
+})
+
+router.post('/teacherIsUsername', function(req, res, next) {
+    var username = req.body.username;
+    let query = "SELECT * FROM teachers WHERE username='"+username+"'";
     con.query(query, (err, result) => {
         if(err) throw err;
         else {
